480b172c25bd67f3a02aed700d7c88ea354f7866
Manuel Sanchís <msanchis@edemy.es>
2015-07-14 11:10:10 +0200

    beta

--- Controller/LinkController.php
+++ Controller/LinkController.php
@@ -2,11 +2,12 @@
 
 namespace eDemy\LinkBundle\Controller;
 
+use Symfony\Component\EventDispatcher\GenericEvent;
 use eDemy\MainBundle\Controller\BaseController;
 use eDemy\MainBundle\Event\ContentEvent;
-
 use eDemy\LinkBundle\Entity\Link;
 use eDemy\LinkBundle\Form\LinkType;
+use eDemy\MainBundle\Entity\Param;
 
 class LinkController extends BaseController
 {
@@ -16,7 +17,25 @@ class LinkController extends BaseController
             'edemy_link_frontpage_lastmodified' => array('onFrontpageLastModified', 0),
             'edemy_link_frontpage' => array('onFrontpage', 0),
             //'edemy_agenda_actividad_details' => array('onActividadDetails', 0),
+            'edemy_mainmenu'                        => array('onLinkMainMenu', 0),
         ));
+    }
+
+    public function onLinkMainMenu(GenericEvent $menuEvent) {
+        $items = array();
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            $item = new Param($this->get('doctrine.orm.entity_manager'));
+            $item->setName('Admin_Link');
+            if($namespace = $this->getNamespace()) {
+                $namespace .= ".";
+            }
+            $item->setValue($namespace . 'edemy_link_link_index');
+            $items[] = $item;
+        }
+
+        $menuEvent['items'] = array_merge($menuEvent['items'], $items);
+
+        return true;
     }
 
     public function onFrontpageLastModified(ContentEvent $event)
